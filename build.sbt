2bb1abf74e05ffeca1f1e5c35210741d5b84c203
Naoki Takezoe <takezoe@gmail.com>
2019-04-14 16:47:44 +0900

    Bump to 1.7.1

--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
 name := "gitbucket-notifications-plugin"
 organization := "io.github.gitbucket"
-version := "1.7.0"
+version := "1.7.1"
 scalaVersion := "2.12.7"
 gitbucketVersion := "4.30.0"
--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -31,7 +31,8 @@ class Plugin extends gitbucket.core.plugin.Plugin {
     new Version("1.5.0"),
     new Version("1.5.1"),
     new Version("1.6.0"),
-    new Version("1.7.0")
+    new Version("1.7.0"),
+    new Version("1.7.1")
   )
 
   override val controllers = Seq(
